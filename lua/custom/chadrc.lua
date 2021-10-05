8d9157f05ff22c07d0a34bb3943c83dc2653ed44
Rich Lively <richielively@gmail.com>
2021-10-05 00:08:13 -0400

    Start personal conf (relative and 4 for tab)

---
+++ lua/custom/chadrc.lua
@@ -0,0 +1,44 @@
+-- IMPORTANT NOTE : This is the user config, can be edited. Will be preserved if updated with internal updater
+-- This file is for NvChad options & tools, custom settings are split between here and 'lua/custom/init.lua'
+
+local M = {}
+M.options, M.ui, M.mappings, M.plugins = {}, {}, {}, {}
+
+--------------------------------------------------------------------
+
+-- To use this file, copy the strucutre of `core/default_config.lua`,
+-- examples of setting relative number & changing theme:
+
+-- M.options = {
+--    relativenumber = true,
+-- }
+M.options = {
+    relativenumber = true,
+    shiftwidth = 4,
+}
+
+-- M.ui = {
+--   theme = "nord"
+-- }
+
+-- NvChad included plugin options & overrides
+M.plugins = {
+   status = {
+     dashboard = true,
+   },
+   options = {
+      --   lspconfig = 
+      --    path of file containing setups of different lsps (ex : "custom.plugins.lspconfig"), read the docs for more info
+      --    nf = "",
+      --   },
+   },
+   -- To change the Packer `config` of a plugin that comes with NvChad,
+   -- add a table entry below matching the plugin github name
+   --              '-' -> '_', remove any '.lua', '.nvim' extensions
+   -- this string will be called in a `require`
+   --              use "(custom.configs).my_func()" to call a function
+   --              use "custom.blankline" to call a file
+   default_plugin_config_replace = {},
+}
+
+return M
